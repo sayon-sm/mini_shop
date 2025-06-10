e4e9c62bc8ccd8040846e37cfecca2e4c6c1686c
sm <its.sayon.mitra@gmail.com>
2025-06-10 21:29:56 +0530

    adding and rendering products using OOP

---
+++ assets/scripts/app.js
@@ -0,0 +1,49 @@
+class ProductItem {
+  constructor(name, price, description, imageUrl) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+}
+
+class ProductList {
+  products = [
+    new ProductItem(
+      'clock',
+      50,
+      'a modern clock with minimilist design',
+      'assets/images/clock.jpg'
+    ),
+    new ProductItem(
+      'pillow',
+      30,
+      'a confortable pillow for a good night sleep',
+      'assets/images/pillow.jpg'
+    ),
+  ];
+
+  render() {
+    const app = document.getElementById('app');
+    const products = document.createElement('ul');
+    products.className = 'products-list';
+    this.products.forEach((product) => {
+      const productItem = document.createElement('li');
+      productItem.className = 'product-item';
+      productItem.innerHTML = `
+      <div class="product-image">
+        <img src="${product.imageUrl}" alt="${product.name}">
+      </div>
+      <div class="product-item__content">
+        <h2>${product.name}</h2>
+        <p>${product.description}</p>
+        <span>$${product.price}</span>
+      </div>
+      `;
+      products.appendChild(productItem);
+    });
+    app.appendChild(products);
+  }
+}
+
+new ProductList().render();
